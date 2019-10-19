d00fbd65ce851b4879c9d47937a9047f432fe1cb
Tony Thomas <thoma127@umn.edu>
2019-10-18 21:17:53 -0500

    Refactor app
    Make app more aware of its elements and add a listener for a custom
    event sent from Slider.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,67 @@
 const Slider = require('./slider');
-const sliders = Array.from(document.getElementsByClassName("input-range"));
 
 class App {
-  constructor(sliderElems) {
-    this.sliders = sliderElems.map((slider) => {
-      new Slider(slider).init();
+  constructor() {
+    this.volumeSlider.init();
+    this.attackSlider.init();
+    this.decaySlider.init();
+    this.sustainSlider.init();
+    this.volumeElem.addEventListener("sliderChange", (event) => {
+      this.onVolumeChange(event);
     });
+  }
+
+  onVolumeChange(event) {
+    console.log(event);
+  }
+
+  get volumeSlider() {
+   return new Slider(this.volumeElem);
+  }
+
+  get volumeValue() {
+    return this.volumeSlider.value;
+  }
+
+  get volumeElem() {
+    return document.querySelector(".input-range.volume");
+  }
+
+  get attackSlider() {
+   return new Slider(this.attackElem);
+  }
+
+  get attackValue() {
+    return this.attackSlider.value;
+  }
+
+  get attackElem() {
+    return document.querySelector(".input-range.attack");
+  }
+
+  get decaySlider() {
+   return new Slider(this.decayElem);
+  }
+
+  get decayValue() {
+    return this.decaySlider.value;
+  }
+
+  get decayElem() {
+    return document.querySelector(".input-range.decay");
+  }
+
+  get sustainSlider() {
+   return new Slider(this.sustainElem);
+  }
+
+  get sustainValue() {
+    return this.sustainSlider.value;
+  }
+
+  get sustainElem() {
+    return document.querySelector(".input-range.sustain");
   }
 }
 
-const app = new App(sliders);
+const app = new App();
